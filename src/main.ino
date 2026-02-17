6b23d4c274be60d01b2ae25206d7c84f413e6017
Kosyo123 <kosyo.mihaylov0809@gmail.com>
2026-02-17 19:24:12 +0200

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,1 +1,28 @@
+int pirPin = 2;
+int lampPin = 8;
+
+unsigned long lastMotionTime = 0;     
+const unsigned long holdTime = 5000;  
+void setup() {
+  pinMode(pirPin, INPUT);
+  pinMode(lampPin, OUTPUT);
+  digitalWrite(lampPin, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int motion = digitalRead(pirPin);
+
+  if (motion == HIGH) {
+    
+    digitalWrite(lampPin, HIGH);
+    lastMotionTime = millis();
+    Serial.println("Dvijenie!");
+  }
+
+  
+  if (millis() - lastMotionTime >= holdTime) {
+    digitalWrite(lampPin, LOW);
+  }
+}
 
